2533c17bd6899cf16e3fb521633f9717cf5dff69
tmunkhb <tmunkhb@gmail.com>
2022-08-21 13:51:25 -0400

    prompt for password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,25 @@
 // Assignment code here
 function generatePassword() {
-
-
+  //Prompt for length
+  while (true) {
+      
+    var userInput = window.prompt("How long do you want your password to be?")
+        
+    // If user canceled the prompt
+    if (userInput === null) {
+    return
+    }
+            
+    var passwordLength = parseInt(userInput)
+        
+    if (isNaN(passwordLength)) {
+    window.alert("That's not a number!")
+    } else if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Invalid password length. Length should be between 8 and 128 characters.")
+    } else {
+    break
+    }
+  }
 
 
 }
